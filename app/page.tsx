31c7e471025ac25761322247083bb46a19c7dc5b
adeebur_x64.efi <domos.exe@gmail.com>
2026-08-19 20:37:21 +0530

    setup: base files

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="justify-center items-center flex p-auto m-auto w-[calc(100vw-64px)] h-[calc(100vh-64px)] backdrop-blur-2xl bg-gray-600/30 rounded-2xl">
+      <h1 className="font-semibold text-3xl bg-gray-500/30 p-2 rounded-xl">adeebur_x64.efi</h1>
+    </main>
+  )
+}
