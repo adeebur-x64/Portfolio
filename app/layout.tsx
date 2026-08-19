817e09284a12a66f08a0e5eee7ede8889ba6e2bd
adeebur_x64.efi <domos.exe@gmail.com>
2026-08-19 22:36:56 +0530

    add: pfp + details + icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const redHatDisplay = Red_Hat_Display({
   variable: "--font-red-hat-display",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,10 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${redHatDisplay.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${redHatDisplay.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import AnimatedBackground from "./AnimatedBackground";
 import Image from "next/image";
 import pfp from "../src/pfp.jpg";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faYoutube,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Page() {
   return (
     <>
@@ -22,8 +29,8 @@ export default function Page() {
           <div className="bg-gray-500/70 rounded-full w-40 h-40 overflow-hidden border-[#2e2e2e] border-2">
             <Image
               src={pfp}
-              width={3840}
-              height={2160}
+              width={2560}
+              height={1440}
               alt="Profile Picture"
               className="object-cover w-full h-full"
             />
@@ -31,10 +38,35 @@ export default function Page() {
           <h1 className="font-semibold text-3xl bg-gray-500/30 p-2 rounded-xl hover:underline hover:decoration-solid hover:text-blue-300 cursor-default">
             adeebur_x64.efi
           </h1>
-          <div className="flex absolute bottom-10 left-auto right-auto gap-10 text-lg">
-            <a href="https://github.com/adeebur-x64">GitHub</a>
-            <a href="https://www.youtube.com/@domos000">YouTube</a>
-            <a href="https://www.x.com/domos000">Twitter / X</a>
+          <div className="font-medium text-md bg-gray-900/70 pl-10 pr-10 pt-5 pb-5 rounded-xl text-[#0FBF3E] w-[calc(100vw-30vw)] lg:w-[calc(100vw-70vw)] md:w-[calc(100vw-60vw)] sm:w-[calc(100vw-60vw)] border-[#2e2e2e] border-2">
+            <h2>
+              &gt; Hi There! I'm Adeebur, aka domos000, a student from India who
+              likes technology! <br /> &gt; I know HTML, CSS, Python, Next.JS
+              and TailwindCSS. <br /> &gt; I'm currently learning C and I like
+              Arch btw :3
+            </h2>
+          </div>
+          <div className="flex absolute bottom-10 left-auto right-auto gap-5 text-lg sm:gap-5 md:gap-5 lg:gap-10">
+            <div className="hover:text-[#0FBF3E] cursor-pointer">
+              <a href="https://github.com/adeebur-x64">
+                <FontAwesomeIcon icon={faGithub} />{" "}
+                <p className="hidden sm:inline-flex hover:underline">GitHub</p>
+              </a>
+            </div>
+            <div className="hover:text-[#FF0000] hover:underline cursor-pointer">
+              <a href="https://www.youtube.com/@domos000">
+                <FontAwesomeIcon icon={faYoutube} />{" "}
+                <p className="hidden sm:inline-flex hover:underline">YouTube</p>
+              </a>
+            </div>
+            <div className="hover:text-[#1DA1F2] hover:underline cursor-pointer">
+              <a href="https://www.x.com/domos000">
+                <FontAwesomeIcon icon={faXTwitter} />{" "}
+                <p className="hidden sm:inline-flex hover:underline">
+                  Twitter / X
+                </p>
+              </a>
+            </div>
           </div>
         </div>
       </main>
